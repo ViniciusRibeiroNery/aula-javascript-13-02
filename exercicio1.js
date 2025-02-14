dd5f9aab98c9f3359937423ac6fde2d1ba276641
ViniciusRibeiroNery <vininery2006@gmail.com>
2025-02-13 22:49:17 -0300

    Criação de um html para testar os códigos e criação de estruturas de controle(Exercício 3).

--- exercicio1.js
+++ exercicio1.js
@@ -2,7 +2,7 @@ const nome = "Vinicius";
 let idade = "18";
 let aprovado = true;
 let notas = [8, 8.5, 10];
-let usuario ={email: "vinicius@exemplo.com.br", senha:"54321"};
+let usuario = {email: "vinicius@exemplo.com.br", senha:"54321"};
 
 console.log("Nome: " + nome, "Idade: " + idade, "Situação: " + aprovado, "Notas: " + notas, "Email/senha: " + usuario);
 
