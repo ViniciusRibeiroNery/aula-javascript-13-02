f0ddf830b0bf76f9492b02c89368bb6f7199b991
ViniciusRibeiroNery <vininery2006@gmail.com>
2025-02-13 22:02:01 -0300

    Adição de comentários sobre a diferença entre "Null" e "Undefined"(Exercício 1).

--- execercio1.js
+++ execercio1.js
@@ -5,3 +5,10 @@ let notas = [8, 8.5, 10];
 let usuario ={email: "vinicius@exemplo.com.br", senha:"54321"};
 
 console.log("Nome: " + nome, "Idade: " + idade, "Situação: " + aprovado, "Notas: " + notas, "Email/senha: " + usuario);
+
+/*                     
+                            Explicação sobre a diferança de "null" e "undefined" 
+
+                    "Null" - Utilizado para indicar que uma variavel não tem valor ou quando uma variavel deve ser zerada.
+                    "undefined" - Utilizado para indicar uma variavel que não foi defininda ainda ou que não existe. 
+*/ 
